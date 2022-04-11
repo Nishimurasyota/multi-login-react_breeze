f7798e612ffe800c7a5d05a1a152feebea06345b
nishimurasyota <syota4723@gmail.com>
2022-04-11 19:07:00 +0900

    :sparkles: ルーティングの設定

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+// Admin用のルーティングを読み込む
+require __DIR__.'/admin.php';
